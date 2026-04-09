0e1c91385ea2643929da9ab14534cdffb2422091
Laszlo Pere <226979892+laszlopere@users.noreply.github.com>
2026-04-09 19:03:50 +0200

    Small changes

--- examples/glade/glade-entries_functions.sh
+++ examples/glade/glade-entries_functions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
-gtkdialog --glade-xml=glade-entries_functions.glade \
+gtkdialog3 --glade-xml=glade-entries_functions.glade \
           --include=glade-entries_functions.functions \
           --program=login_window
--- examples/glade/glade-toolbar_buttons.sh
+++ examples/glade/glade-toolbar_buttons.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gtkdialog --glade-xml=glade-toolbar_buttons.glade \
+gtkdialog3 --glade-xml=glade-toolbar_buttons.glade \
           --program=MAIN_WINDOW
